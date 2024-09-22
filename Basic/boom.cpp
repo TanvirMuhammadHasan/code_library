caa83ccd3b4eee98c554739c311154fb1910d360
TanvirMuhammadHasan <tanvirhasan.2307@gmail.com>
2024-09-22 12:59:51 +0600

    Create boom.cpp

---
+++ Basic/boom.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Start ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define bug(a) cout<< #a << ':' << a << endl;
+const ll INF= LONG_LONG_MAX;
+const int N = 1e5+10;
+const double pi = 2 * acos (0.0);
+const ll mod=1e9+7;
+
+void solve(int test)
+{
+
+}
+int main()
+{
+  Start
+  int t;
+  t=1;  
+  cin >> t;
+  for(int test = 1;test <= t;++test) solve(test);
+}
